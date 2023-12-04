1e3cd2679005b82c5e70777fd6349592f4530a90
obie <obieomens@gmail.com>
2023-12-04 11:47:19 +0100

    no C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''.join(char for char in my_string if char.lower() not in ('c', 'C'))
+
+    new_string = ""
+
+    for elements in my_string:
+
+        if elements != "c" and elements != "C":
+
+            new_string += elements
+
     return new_string

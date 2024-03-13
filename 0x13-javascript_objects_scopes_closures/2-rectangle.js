82629ba3495ee01aea010e4cabe01e00ec60f54b
obieomens <obieomens@gmail.com>
2024-03-13 02:35:07 -0700

    rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)){ 
+    if ((w > 0) && (h > 0)) { 
       this.width = w;
       this.hieght = h;
     }
   }
 }
+
 module.exports = Rectangle;
